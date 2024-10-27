a09eaeae838b3c3e0cfcea4614bcf1dd6d728722
Mjenius <mjenius1357@gmail.com>
2024-10-27 21:21:46 +0530

    Header and Home Page

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,30 +1,30 @@
-import React, { useState } from 'react'
-import './Navbar.css'
-import { assets } from '../../assets/assets'
+import React, { useState } from 'react';
+import './Navbar.css';
+import { assets } from '../../assets/assets';
 
 const Navbar = () => {
+    const [menu, setMenu] = useState("home"); // Changed default to "home"
 
-    const [menu,setMenu] = useState("menu")
-
-  return (
-    <div className='navbar'>
-      <img src={assets.logo} alt='logo' className='logo'/>
-      <ul className='navbar-menu'>
-        <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</li>
-        <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</li>
-        <li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</li>
-        <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</li>
-      </ul>
-      <div className='navbar-right'>
-        <img src={assets.search_icon} alt=''/>
-        <div className='navbar-search-icon'>
-            <img src={assets.basket_icon} alt=""/>
-            <div className='dot'></div>
+    return (
+        <div className='navbar'>
+            <img src={assets.logo} alt='logo' className='logo' />
+            <ul className='navbar-menu'>
+                {["home", "menu", "mobile-app", "contact-us"].map(item => (
+                    <li key={item} onClick={() => setMenu(item)} className={menu === item ? "active" : ""}>
+                        {item.replace("-", " ")} {/* Format for display */}
+                    </li>
+                ))}
+            </ul>
+            <div className='navbar-right'>
+                <img src={assets.search_icon} alt='Search' aria-hidden="true" />
+                <div className='navbar-search-icon'>
+                    <img src={assets.basket_icon} alt="Basket" />
+                    <div className='dot'></div>
+                </div>
+                <button aria-label="Sign In">Sign In</button>
+            </div>
         </div>
-        <button>Sign In</button>
-      </div>
-    </div>
-  )
+    );
 }
 
-export default Navbar
+export default Navbar;
