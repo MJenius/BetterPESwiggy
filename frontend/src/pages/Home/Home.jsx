3f7f53ad43d44617e4d8755fa20bbb0e21c838ec
Mjenius <mjenius1357@gmail.com>
2024-10-28 11:28:23 +0530

    Login Popup and Screen Sizing

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -4,6 +4,8 @@ import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import { useState } from 'react'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+import AppDownload from '../../components/AppDownload/AppDownload'
+
 const Home = () => {
 
   const [category,setCategory] = useState("All");
@@ -13,6 +15,7 @@ const Home = () => {
       <Header/>
       <ExploreMenu category={category} setCategory={setCategory}/>
       <FoodDisplay category={category}/>
+      <AppDownload/>
     </div>
   )
 }
